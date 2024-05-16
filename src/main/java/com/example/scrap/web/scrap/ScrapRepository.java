dfcd55a0b987e7e9c75d9f5a5bc2323ece66c967
choign3123 <choign3123@naver.com>
2024-05-16 15:25:36 +0900

    remove: 불필요한 import 삭제

--- src/main/java/com/example/scrap/web/scrap/ScrapRepository.java
+++ src/main/java/com/example/scrap/web/scrap/ScrapRepository.java
@@ -1,12 +1,6 @@
 package com.example.scrap.web.scrap;
 
-import com.example.scrap.entity.Category;
-import com.example.scrap.entity.Member;
 import com.example.scrap.entity.Scrap;
-import com.example.scrap.entity.enums.ScrapStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
