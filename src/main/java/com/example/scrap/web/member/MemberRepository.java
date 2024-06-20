2c7aa2bf19b195161c937bddd0152e0c16bab3ea
choign3123 <choign3123@naver.com>
2024-06-20 17:43:04 +0900

    update: 마이페이지 API 응답값 수정
    - #98

--- src/main/java/com/example/scrap/web/member/MemberRepository.java
+++ src/main/java/com/example/scrap/web/member/MemberRepository.java
@@ -2,6 +2,7 @@ package com.example.scrap.web.member;
 
 import com.example.scrap.entity.Member;
 import com.example.scrap.entity.enums.SnsType;
+import com.example.scrap.web.mypage.dto.MypageResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +13,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findBySnsTypeAndSnsId(SnsType snsType, String snsId);
 
+    // [TODO] 좀더 유지보수하기 좋은 코드가 없는지 고민 필요
+    @Query("SELECT new com.example.scrap.web.mypage.dto.MypageResponse$MypageDTO$Statistics(count(c), count(s)) " + // inner 클래스의경우 .이 아닌 $로 표시
+            "FROM Category c " +
+            "LEFT jOIN c.scrapList s " +
+            "WHERE c.member =:member " +
+            "AND (s.status = 'ACTIVE' OR s.status is NULL)")
+    MypageResponse.MypageDTO.Statistics getMypageStatistics(@Param("member") Member member);
 }
--- src/main/java/com/example/scrap/web/mypage/MypageQueryServiceImpl.java
+++ src/main/java/com/example/scrap/web/mypage/MypageQueryServiceImpl.java
@@ -2,14 +2,11 @@ package com.example.scrap.web.mypage;
 
 import com.example.scrap.converter.MypageConverter;
 import com.example.scrap.entity.Member;
-import com.example.scrap.entity.Scrap;
-import com.example.scrap.specification.ScrapSpecification;
 import com.example.scrap.web.member.IMemberQueryService;
+import com.example.scrap.web.member.MemberRepository;
 import com.example.scrap.web.member.dto.MemberDTO;
-import com.example.scrap.web.mypage.dto.MypageResponse;
-import com.example.scrap.web.scrap.ScrapRepository;
+import com.example.scrap.web.mypage.dto.MypageResponse.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,21 +16,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class MypageQueryServiceImpl implements IMypageQueryService {
 
     private final IMemberQueryService memberService;
-    private final ScrapRepository scrapRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 마이페이지 조회
      * @param memberDTO
      * @return
      */
-    public MypageResponse.MypageDTO mypage(MemberDTO memberDTO){
+    public MypageDTO mypage(MemberDTO memberDTO){
         Member member = memberService.findMember(memberDTO);
 
-        Specification<Scrap> spec = Specification.where(ScrapSpecification.isAvailable())
-                .and(ScrapSpecification.equalMember(member));
-
-        int totalScrap = Long.valueOf(scrapRepository.count(spec)).intValue();
-
-        return MypageConverter.toMypage(totalScrap);
+        MypageDTO.Statistics statistics = memberRepository.getMypageStatistics(member);
+        return MypageConverter.toMypage(member, statistics);
     }
 }
