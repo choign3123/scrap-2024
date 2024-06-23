0ce5fdf46f17c50f0949661cc7857951d6439a45
choign3123 <choign3123@naver.com>
2024-06-23 16:49:56 +0900

    remove: LoginAndLogoutStatusInterceptor 삭제
    - 로그아웃 상태에서도 사용 가능한 API가 없어서 삭제

--- src/main/java/com/example/scrap/config/AppConfig.java
+++ src/main/java/com/example/scrap/config/AppConfig.java
@@ -1,12 +1,10 @@
 package com.example.scrap.config;
 
 import com.example.scrap.interceptor.AuthorizationInterceptor;
-import com.example.scrap.interceptor.LoginAndLogoutStatusInterceptor;
 import com.example.scrap.interceptor.LoginStatusInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,7 +15,6 @@ public class AppConfig implements WebMvcConfigurer {
 
     private final AuthorizationInterceptor authorizationInterceptor;
     private final LoginStatusInterceptor loginStatusInterceptor;
-    private final LoginAndLogoutStatusInterceptor loginAndLogoutStatusInterceptor;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -42,14 +39,5 @@ public class AppConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/oauth/login/**")
                 .excludePathPatterns("/me")
                 .excludePathPatterns("/oauth/naver/callback");
-
-        // 로그아웃 상태에서 사용 가능한 API
-        registry.addInterceptor(loginAndLogoutStatusInterceptor)
-                .order(3) // OpenEntityManagerInViewInterceptor 를 먼저 동작시키게 하기 위해서 우선순위 낮춤.
-                .addPathPatterns("/me")
-                .excludePathPatterns("/oauth/login/**")
-                .excludePathPatterns("/oauth/naver/callback");
-
-        // [TODO] 로그인, 토큰 유효성 조회 API는 빼기
     }
 }
