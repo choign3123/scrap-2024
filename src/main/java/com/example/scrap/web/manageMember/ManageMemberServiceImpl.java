8913debc008ad983e00adea846cbadcddf941ae6
choign3123 <choign3123@naver.com>
2024-06-24 12:52:42 +0900

    fix: access토큰이 만료됐는데, refresh토큰을 갱신했던 문제 해결
    - #111

--- src/main/java/com/example/scrap/web/manageMember/ManageMemberServiceImpl.java
+++ src/main/java/com/example/scrap/web/manageMember/ManageMemberServiceImpl.java
@@ -27,16 +27,23 @@ public class ManageMemberServiceImpl implements IMangeMemberService{
      * 토큰 유효성 검사
      */
     public Token validateToken(ManageMemberRequest.ValidateTokenDTO request){
+        return validateToken(request.getAccessToken(), request.getRefreshToken());
+    }
+
+    /**
+     * 토큰 유효성 검사
+     */
+    public Token validateToken(String accessToken, String refreshToken){
         boolean isAccessValid, isRefreshValid;
         Token token;
 
         // 토큰 만료 여부 확인
-        isAccessValid = tokenProvider.isTokenValid(request.getAccessToken());
-        isRefreshValid = tokenProvider.isTokenValid(request.getRefreshToken());
+        isAccessValid = tokenProvider.isTokenValid(accessToken);
+        isRefreshValid = tokenProvider.isTokenValid(refreshToken);
 
         token = Token.builder()
-                .accessToken(request.getAccessToken())
-                .refreshToken(request.getRefreshToken())
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
                 .build();
 
         // 토큰이 유효할 때 공통적으로 처리해야 되는 작업 수행
@@ -56,11 +63,6 @@ public class ManageMemberServiceImpl implements IMangeMemberService{
             if(!tokenProvider.isTokenTypeIsRefresh(token.getRefreshToken())){
                 throw new AuthorizationException(ErrorCode.NOT_REFRESH_TOKEN);
             }
-
-            // refresh 갱신이 필요한 경우
-            if(tokenProvider.isRequiredTokenReissue(token.getRefreshToken())){
-                token = tokenProvider.reissueRefreshToken(token);
-            }
         }
 
         // 각 상황에 대해 특별히 필요한 작업 수행
@@ -71,6 +73,11 @@ public class ManageMemberServiceImpl implements IMangeMemberService{
             // access와 refresh의 멤버가 같은지 확인
             if(!tokenProvider.isMemberOfTokenSame(token)){
                 throw new AuthorizationException(ErrorCode.ACCESS_MEMBER_AND_REFRESH_MEMBER_NOT_MATCH);
+            }
+
+            // refresh 갱신이 필요한 경우
+            if(tokenProvider.isRequiredTokenReissue(token.getRefreshToken())){
+                token = tokenProvider.reissueRefreshToken(token);
             }
         }
         else if(isAccessValid == false && isRefreshValid == true){ // access[X] refresh[O]
