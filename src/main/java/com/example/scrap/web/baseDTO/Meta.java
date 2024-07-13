1983f154734a2ea8e99a61ddfcf2a33200770d76
choign3123 <choign3123@naver.com>
2024-07-13 13:37:21 +0900

    update: meta값 수정
    - #132
    - 현재 페이지 -> 현재 페이지에 나올 데이터 수로 변경
    - 변수명 수정

--- src/main/java/com/example/scrap/web/baseDTO/Meta.java
+++ src/main/java/com/example/scrap/web/baseDTO/Meta.java
@@ -1,19 +1,18 @@
 package com.example.scrap.web.baseDTO;
 
-import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
 @Getter
 public class Meta {
 
-    private int totalCount;
-    private int currentPage;
+    private long totalElemnt; // 총 테이터 개수
+    private int numOfElement; // 현재 페이지에 나올 데이터 수
     private Boolean isEnd;
 
     public Meta(Page page){
-        this.totalCount = Long.valueOf(page.getTotalElements()).intValue();
-        this.currentPage = page.getPageable().getPageNumber();
+        this.totalElemnt = page.getTotalElements();
+        this.numOfElement = page.getNumberOfElements();
         this.isEnd = page.isLast();
     }
 }
