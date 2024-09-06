6f9a5d524ced3dfb74c1bf6c090fd11d3ec84f42
choign3123 <choign3123@naver.com>
2024-09-06 11:02:31 +0900

    CHORE: dev-deploy.sh 도커 실행 명령어 수정
    - #166
    - 도커에 실행 권한 줌

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -2,7 +2,7 @@ sudo docker pull $DOCKER_REPOSITORY_URL:dev
 
 if [ "$(docker ps -q -f name=$DOCKER_DEV_CONTAINER_NAME)" ]; then
   echo "Stopping container: $DOCKER_DEV_CONTAINER_NAME"
-  docker stop $DOCKER_DEV_CONTAINER_NAME
+  sudo docker stop $DOCKER_DEV_CONTAINER_NAME
 else
   echo "Container $DOCKER_DEV_CONTAINER_NAME is not running."
 fi
