67cbe577bac743b0d0552fb1d120dea54b0f27ce
choign3123 <choign3123@naver.com>
2024-09-08 18:43:49 +0900

    CHORE: aws parameter store를 사용하여 민감한 정보 처리 적용중(14)
    - #166

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -9,19 +9,13 @@ export EXPIRE_DAY_OF_REFRESH=$(aws ssm get-parameters --names /Dev/Jwt/expire_da
 export REDIS_HOST=$(aws ssm get-parameters --names /Dev/Redis/redis_host --with-decryption --query Parameters[0].Value)
 export REDIS_PORT=$(aws ssm get-parameters --names /Dev/Redis/redis_port --with-decryption --query Parameters[0].Value)
 
-echo "test env print at next line:"
-echo $DEV_DB_USERNAME
-printenv
-
-exit 1;
-
-#sudo docker run --name scrap2024dev \
-#-e DEV_REDIS_HOST=$DEV_REDIS_HOST \
-#-e DEV_REDIS_PORT=$DEV_REDIS_PORT \
-#-e DEV_DB_URL=$DEV_DB_URL \
-#-e DEV_DB_USERNAME=$DEV_DB_USERNAME \
-#-e DEV_DB_PASSWORD=$DEV_DB_PASSWORD \
-#-e JWT_SECRET=$JWT_SECRET \
-#-e EXPIRE_HOUR_OF_ACCESS=$EXPIRE_HOUR_OF_ACCESS \
-#-e EXPIRE_DAY_OF_REFRESH=$EXPIRE_DAY_OF_REFRESH \
-#choign3123/scrap2024:dev
+sudo docker run --name scrap2024dev \
+-e DEV_REDIS_HOST=$DEV_REDIS_HOST \
+-e DEV_REDIS_PORT=$DEV_REDIS_PORT \
+-e DEV_DB_URL=$DEV_DB_URL \
+-e DEV_DB_USERNAME=$DEV_DB_USERNAME \
+-e DEV_DB_PASSWORD=$DEV_DB_PASSWORD \
+-e JWT_SECRET=$JWT_SECRET \
+-e EXPIRE_HOUR_OF_ACCESS=$EXPIRE_HOUR_OF_ACCESS \
+-e EXPIRE_DAY_OF_REFRESH=$EXPIRE_DAY_OF_REFRESH \
+choign3123/scrap2024:dev
