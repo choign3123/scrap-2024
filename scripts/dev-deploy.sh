0dc7a8908a9840bf13a3887a2de6689b338b9378
choign3123 <choign3123@naver.com>
2024-09-09 17:24:55 +0900

    FIX: 환경변수 설정은 insert-dev-environment.sh로, 도커 배포는 dev-deploy.sh로 코드 분리
    - #166

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -1,16 +1,5 @@
 sudo docker pull choign3123/scrap2024:dev
 
-export DB_URL=$(aws ssm get-parameters --names /Dev/Mysql/db_url --with-decryption --query Parameters[0].Value --output text)
-export DB_USERNAME=$(aws ssm get-parameters --names /Dev/Mysql/db_username --with-decryption --query Parameters[0].Value --output text)
-export DB_PASSWORD=$(aws ssm get-parameters --names /Dev/Mysql/db_password --with-decryption --query Parameters[0].Value --output text)
-export JWT_SECRET=$(aws ssm get-parameters --names /Dev/Jwt/jwt_secret_key --with-decryption --query Parameters[0].Value --output text)
-export EXPIRE_HOUR_OF_ACCESS=$(aws ssm get-parameters --names /Dev/Jwt/expire_hour_of_access --with-decryption --query Parameters[0].Value --output text)
-export EXPIRE_DAY_OF_REFRESH=$(aws ssm get-parameters --names /Dev/Jwt/expire_day_of_refresh --with-decryption --query Parameters[0].Value --output text)
-export REDIS_HOST=$(aws ssm get-parameters --names /Dev/Redis/redis_host --with-decryption --query Parameters[0].Value --output text)
-export REDIS_PORT=$(aws ssm get-parameters --names /Dev/Redis/redis_port --with-decryption --query Parameters[0].Value --output text)
-export BASE_PORT=$(aws ssm get-parameters --names /Dev/port --with-decryption --query Parameters[0].Value --output text)
-
-
 sudo docker run --name scrap2024dev \
 -e REDIS_HOST=$REDIS_HOST \
 -e REDIS_PORT=$REDIS_PORT \
