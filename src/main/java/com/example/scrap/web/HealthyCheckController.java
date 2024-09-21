0305bd04b0da688f61fd41ec790f367212cc811f
choign3123 <choign3123@naver.com>
2024-09-21 17:41:11 +0900

    UPDATE: Health check API의 리턴값에 localDateTime값 추가
    - #189

--- src/main/java/com/example/scrap/web/HealthyCheckController.java
+++ src/main/java/com/example/scrap/web/HealthyCheckController.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @Tag(name = "Healthy Check", description = "Health Check 관련 API")
 public class HealthyCheckController {
@@ -14,6 +16,6 @@ public class HealthyCheckController {
     )
     @GetMapping("/health")
     public String checkHealth(){
-        return "I'm healthy! from 24.09.10";
+        return "I'm healthy! " + LocalDateTime.now();
     }
 }
