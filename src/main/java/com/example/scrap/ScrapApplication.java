c5db4fe2f7cdeeb4de4c94d78d9dc4aa7ba7cf37
choign3123 <choign3123@naver.com>
2024-09-29 16:10:47 +0900

    COMMENT: 테스트 주석 작성

--- src/main/java/com/example/scrap/ScrapApplication.java
+++ src/main/java/com/example/scrap/ScrapApplication.java
@@ -14,6 +14,8 @@ public class ScrapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ScrapApplication.class, args);
 		System.setProperty(DefaultData.DEPLOY_TIME_KEY, LocalDateTime.now().toString());
+
+		// 테스트 주석 작성
 	}
 
 }
